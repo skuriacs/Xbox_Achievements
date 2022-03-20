a36ca75810f6dc4f9f0ae5176e6db87ec9547720
Sam <skuriacs@gmail.com>
2022-03-19 22:04:55 -0400

    Revamped element search to find game stats, should break less often

--- Xbox.py
+++ Xbox.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 import os
 import pandas as pd
 import sys
@@ -31,48 +32,60 @@ cwd = os.getcwd()
 
 
 def return_game_stats(div: str) -> WebDriverWait:
-    return WebDriverWait(driver, 10).until(
-        lambda x: x.find_elements_by_class_name(div)
-    )
+    try: 
+        return WebDriverWait(driver, 20).until(
+            lambda x: x.find_elements(by= By.CLASS_NAME, value = div)
+        )
+    except Exception as e:
+        print("Webdriver probably timed out, no stats found for game")
+        return []
 
 # Tries to get html element that has the minutes played for the game
 
 
-def stat_exist_time(game_stats: list) -> str:
+def stat_exist_time(game_stats: list) -> int:
     try:
-        time_played = game_stats[-1]
-        time_played = time_played.get_attribute(
-            "innerHTML").strip().replace(",", "")
-        if "%" in time_played:
-            time_played = game_stats[-2]
-            time_played = time_played.get_attribute(
-                "innerHTML").strip().replace(",", "")
-        return time_played
+        for game_stat_box in game_stats:
+            if  game_stat_box.find_element(by = By.CLASS_NAME, value= "statlabel").get_attribute("innerHTML") == "Minutes Played":
+                time_played_stat = game_stat_box.find_element(by = By.CLASS_NAME, value= "statdata")
+                time_played = time_played_stat.get_attribute(
+                    "innerHTML").strip().replace(",", "")
+                return int(time_played)
+        return "0"
     except Exception as e:
         print(e)
-        return "0"
+        return 0
 
 # Returns a list of the GamerScore a player has earned and the max possible amount for a game
 
 
 def get_gamerscore(game_stats: list) -> list:
-    try:
-        gamer_score = game_stats[0]
-        gamer_score = gamer_score.get_attribute(
-            "innerHTML").replace(" ", "").replace(",", "").strip().split("/")
-        return gamer_score
-    except Exception as e:
-        print(e)
+    if len(game_stats) < 2:
         return [0, 0]
+
+    for game_stat_box in game_stats:
+        try:
+            if  game_stat_box.find_element(by = By.CLASS_NAME, value= "statlabel").get_attribute("innerHTML") == "Gamerscore":
+                game_score_stat = game_stat_box.find_element(by = By.CLASS_NAME, value= "statdata")
+                gamer_score = game_score_stat.get_attribute(
+                "innerHTML").replace(" ", "").replace(",", "").strip().split("/")
+                return gamer_score
+        except Exception as e:
+            print(e)
+            return [0,0]
+
+    return [0, 0]
 
 # Gets the number of achievements a player has earned
 
 
 def get_achievement_num(game_stats: list) -> int:
     try:
-        achievements = game_stats[1]
-        achievements = achievements.get_attribute("innerHTML").strip()
-        return int(achievements)
+        for game_stat_box in game_stats:
+            if  game_stat_box.find_element(by = By.CLASS_NAME, value= "statlabel").get_attribute("innerHTML") == "Achievements":
+                achieve_stat = game_stat_box.find_element(by = By.CLASS_NAME, value= "statdata")
+                num_achieve_stats = int(achieve_stat.get_attribute("innerHTML"))
+                return num_achieve_stats if num_achieve_stats else 0
     except Exception as e:
         print(e)
         return 0
@@ -82,7 +95,7 @@ def get_achievement_num(game_stats: list) -> int:
 
 def wait_for_id(id: str, time=10) -> WebDriverWait:
     return WebDriverWait(driver, time).until(
-        lambda x: x.find_element_by_id(id)
+        lambda x: x.find_elements(by = By.ID, value = id)
     )
 
 
@@ -99,45 +112,38 @@ except Exception as e:
     driver.quit()
     sys.exit()
 
-games = list_of_games.find_elements_by_tag_name("li")
+games = list_of_games[0].find_elements(by = By.TAG_NAME , value = "li")
 game_urls = []
 game_names = []
 
 # Goes through the game list and appends each href link to game_urls, and appends the name of the game as well
 for game in games:
-    game_wrapper = game.find_element_by_class_name(
-        "recentProgressLinkWrapper")
+    game_wrapper = game.find_element(by = By.CLASS_NAME,
+        value = "recentProgressLinkWrapper")
     url = game_wrapper.get_attribute("href")
     game_urls.append(url)
     game_name = game_wrapper.get_attribute("aria-label")
     game_names.append(game_name)
 
 # Pandas dataframe for storing the info
-df = pd.DataFrame(columns=['Game_Name', 'Minutes_Played',
-                  "GamerScore_Earned", "GamerScore_Possible", "Number_of_Achievements"])
-
 """
 For each url page, try to get the time played in minutes, gamer score, and number of achievements for each game, then append it to the df
 """
-current_game = 1
 total_games = len(game_names)
-for url, name in zip(game_urls, game_names):
+game_stats_all = []
+for url, name, current_game_num in zip(game_urls, game_names, range(1,total_games)):
     driver.get(url)
     print("Getting info for : {:<70}{:<4} out of {:<4}".format(
-        name, current_game, total_games))
-    current_game += 1
-    game_stats = return_game_stats("statdata")
+        name, current_game_num, total_games))
+    game_stats = return_game_stats("stattile")
+    if len(game_stats) == 0:
+        game_stats.append([name,0,0,0,0])
+        continue
     time_played = stat_exist_time(game_stats)
     gamer_score_earned, gamer_score_max = get_gamerscore(game_stats)
     num_of_achievements = get_achievement_num(game_stats)
-    df = df.append(
-        {
-            "Game_Name": name,
-            "Minutes_Played": time_played,
-            "GamerScore_Earned": gamer_score_earned,
-            "GamerScore_Possible": gamer_score_max,
-            "Number_of_Achievements": num_of_achievements
-        }, ignore_index=True)
+    game_stats_all.append([name,time_played,gamer_score_earned,gamer_score_max,num_of_achievements])
+
 
 
 # Some new update caused some numbers to be "---", these lines should replace those values with 0 (11/16/21)
@@ -146,6 +152,11 @@ def replaceDashes(element):
         element = "0"
     return element
 
+df : pd.DataFrame = pd.DataFrame(game_stats_all, columns= ['Game_Name', 'Minutes_Played',
+                  "GamerScore_Earned", "GamerScore_Possible", "Number_of_Achievements"])
+
+
+df = df.fillna(value = 0)
 
 df[["Minutes_Played", "GamerScore_Earned", "GamerScore_Possible", "Number_of_Achievements"]] = df[[
     "Minutes_Played", "GamerScore_Earned", "GamerScore_Possible", "Number_of_Achievements"]].applymap(replaceDashes)
@@ -157,7 +168,7 @@ df[["Minutes_Played", "GamerScore_Earned", "GamerScore_Possible", "Number_of_Ach
 choice = input(
     "Do you want to include games with 0 minutes played in your csv? (Y/N)")
 choice = choice.upper().strip()
-if choice == 'N':
+if choice.capitalize().strip() == 'N':
     zero_minutes_filter = df["Minutes_Played"] != 0
     df = df[zero_minutes_filter]
 
